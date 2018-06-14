6190e1a3e1ead8ea77b7338e72b0fe3aedf9f750
briecodes <hello@brie.work>
2018-06-14 09:23:47 -0400

    finalize before moving to dev branch.

--- src/Components/tubeResult.js
+++ src/Components/tubeResult.js
@@ -4,7 +4,7 @@ const TubeResult = (props) => (
     <div className="individualResult">
         <img src={props.videoInfo.thumbnails.default.url} alt="props.videoInfo.title" className="thumb" />
         <div className="titleContainer">
-            {props.videoInfo.title.substring(0, 38)}
+            {props.videoInfo.title.substring(0, 40)}
         </div>
         <div className="buttonContainer">
             <button type="button" data-videoid={props.videoInfo.id} value={props.videoInfo.link} onClick={props.clickHandler} >Choose Video</button>
